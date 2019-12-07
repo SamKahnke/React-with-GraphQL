f63101b1b0b893c445693bebdeb6e957021677db
SamKahnke <samkahnke@gmail.com>
2019-12-06 19:15:02 -0600

    add Items and createItem

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,6 +1,19 @@
 const Mutations = {
-    createDog(parent, args, ctx, info) {
-        console.log(args);
+    async createItem(parent, args, ctx, info) {
+      // TODO: Check if they are logged in
+  
+      const item = await ctx.db.mutation.createItem(
+        {
+          data: {
+            ...args,
+          },
+        },
+        info
+      );
+  
+      console.log(item);
+  
+      return item;
     },
 };
 
